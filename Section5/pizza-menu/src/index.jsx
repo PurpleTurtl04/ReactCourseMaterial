1f48d7ef5288a350d9b752a8815718a5adda1dca
Nick Edwards <nledwa01@gmail.com>
2025-08-28 16:53:57 -0500

    8-28-25 First session

--- Section5/pizza-menu/src/index.jsx
+++ Section5/pizza-menu/src/index.jsx
@@ -57,16 +57,6 @@ function App() {
   );
 }
 
-function Pizza() {
-  return (
-    <div>
-      <img src='pizzas/spinaci.jpg' alt='Pizza Spinaci' />
-      <h3>Pizza Spinachi</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
-}
-
 function Header() {
   // const style = {color: 'red', fontSize: '48px', textTransform: 'uppercase'}
   return (
@@ -80,11 +70,32 @@ function Menu() {
   return (
     <main className='menu'>
       <h2>Our menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name='Pizza Spinaci'
+        ingredients='Tomato, mozarella, spinach, and ricotta cheese'
+        photoName='pizzas/spinaci.jpg'
+        price={10}
+      />
+      <Pizza
+        name='Pizza Funghi'
+        ingredients='Tomato, mozarella, mushrooms, and onion'
+        photoName='pizzas/funghi.jpg'
+        price={12}
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <div className='pizza'>
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
   );
 }
 
