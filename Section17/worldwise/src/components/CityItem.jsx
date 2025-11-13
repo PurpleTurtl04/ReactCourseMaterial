e86fabf1fbe1ed285320fada031f7ef3697fc41a
Nick Edwards <nledwa01@gmail.com>
2025-11-13 16:39:58 -0600

    URC - Completed videos 230-231 and started 232. Moved Context folder. Added map features

--- Section17/worldwise/src/components/CityItem.jsx
+++ Section17/worldwise/src/components/CityItem.jsx
@@ -1,35 +1,35 @@
-import { Link } from "react-router-dom";
-import styles from "./CityItem.module.css";
-import { useCities } from "../../contexts/CitiesContext";
+import { Link } from 'react-router-dom';
+import styles from './CityItem.module.css';
+import { useCities } from '../contexts/CitiesContext';
 
 const formatDate = (date) =>
-  new Intl.DateTimeFormat("en", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-  }).format(new Date(date));
+    new Intl.DateTimeFormat('en', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+    }).format(new Date(date));
 
 function CityItem({ city }) {
-  const { currentCity } = useCities();
-  const { cityName, emoji, date, id, position } = city;
+    const { currentCity } = useCities();
+    const { cityName, emoji, date, id, position } = city;
 
-  console.log(position);
+    console.log(position);
 
-  return (
-    <li>
-      <Link
-        className={`${styles.cityItem} ${
-          id === currentCity.id ? styles["cityItem--active"] : ""
-        }`}
-        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
-      >
-        <span className={styles.emoji}>{emoji}</span>
-        <h1 className={styles.name}>{cityName}</h1>
-        <time className={styles.date}>{formatDate(date)}</time>
-        <button className={styles.deleteBtn}>&times;</button>
-      </Link>
-    </li>
-  );
+    return (
+        <li>
+            <Link
+                className={`${styles.cityItem} ${
+                    id === currentCity.id ? styles['cityItem--active'] : ''
+                }`}
+                to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+            >
+                <span className={styles.emoji}>{emoji}</span>
+                <h1 className={styles.name}>{cityName}</h1>
+                <time className={styles.date}>{formatDate(date)}</time>
+                <button className={styles.deleteBtn}>&times;</button>
+            </Link>
+        </li>
+    );
 }
 
 export default CityItem;
--- Section17/worldwise/src/components/Map.jsx
+++ Section17/worldwise/src/components/Map.jsx
@@ -1,36 +1,59 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
-import styles from "./Map.module.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useState, useContext } from "react";
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import styles from './Map.module.css';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { useEffect, useState } from 'react';
+import { useCities } from '../contexts/CitiesContext';
 
 function Map() {
-  const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [mapPosition, setMapPosition] = useState([40, 0]);
+    const navigate = useNavigate();
+    const { cities } = useCities();
 
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+    const [searchParams] = useSearchParams();
+    const [mapPosition, setMapPosition] = useState([40, 0]);
 
-  return (
-    <div className={styles.mapContainer}>
-      <MapContainer
-        center={mapPosition}
-        zoom={13}
-        scrollWheelZoom={false}
-        className={styles.Map}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
-  );
+    const mapLat = searchParams.get('lat');
+    const mapLng = searchParams.get('lng');
+
+    useEffect(
+        function () {
+            if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+        },
+        [mapLat, mapLng]
+    );
+
+    return (
+        <div className={styles.mapContainer}>
+            <MapContainer
+                center={mapPosition}
+                zoom={6}
+                scrollWheelZoom={true}
+                className={styles.map}
+            >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                />
+                {cities.map((city) => (
+                    <Marker
+                        position={[city.position.lat, city.position.lng]}
+                        key={city.id}
+                    >
+                        <Popup>
+                            <span>{city.emoji}</span>{' '}
+                            <span>{city.cityName}</span>
+                        </Popup>
+                    </Marker>
+                ))}
+                <ChangeCenter position={mapPosition} />
+            </MapContainer>
+        </div>
+    );
+}
+
+function ChangeCenter({ position }) {
+    const map = useMap();
+    map.setView(position);
+    return null;
 }
 
 export default Map;
